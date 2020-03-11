19dd9bbdf380d25abfcee66ba7aceb38bf7bd143
Pro X <pro@proteanx.com>
2020-03-10 22:10:51 -0400

    v3.1.0 binaries

--- docker/buildandpush.sh
+++
@@ -1,3 +0,0 @@
-docker build -t registry.gitlab.com/bitcorn/corn-docker:1.4.0.3 -t registry.gitlab.com/bitcorn/corn-docker:latest .
-docker push registry.gitlab.com/bitcorn/corn-docker:1.4.0.3
-docker push registry.gitlab.com/bitcorn/corn-docker:latest
