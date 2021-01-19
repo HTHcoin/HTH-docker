225c2fe231fd2e7346621a4fe66cc900745ef7f4
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2021-01-18 23:48:51 -0500

    Renamed for changes
    Renamed bitcorn to helpthehomeless

--- scripts/helpthehomeless_all_params.sh
+++ scripts/helpthehomeless_all_params.sh
@@ -3,8 +3,8 @@
 id=1
 idstring=$(printf '%03d' ${id})
 JSON="[]"
-while [ -f "/etc/systemd/system/bitcorn-${idstring}.service" ]; do
-  NEW=$(sh /opt/bitcorn/chainparams-${idstring}.sh)
+while [ -f "/etc/systemd/system/helpthehomeless-${idstring}.service" ]; do
+  NEW=$(sh /opt/helpthehomeless/chainparams-${idstring}.sh)
   JSON=$(echo $JSON | jq '. += ['"$NEW"']')
 
   id=$((id + 1))
